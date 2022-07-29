994d3f7848285309a1a627913e3729218ec79999
Takeshi-mi <101356765+Takeshi-mi@users.noreply.github.com>
2022-07-29 18:48:33 +0000

    Initial commit

---
+++ Aula 1.java
@@ -0,0 +1,7 @@
+/**
+  WORA, write one, run anywhere
+  Graças ao JVM (Java Virtual Machine)
+  Para que o Java funcione no computador é preciso ter essa   máquina virtual instalada.
+Java está em terceiro no rank da TIOBE
+Bastantes vagas no site APinfo.
+/*
---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+  Hello World
+  @author Takeshi
+*/
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Tenho que escrever um monte de coisa só pra printar. Sdds Python");
+  } 
+}
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("hello world")
+  }
+}
